6b191697f308e49c5325c827ef747e916b67d0fb
Sam Bäumer <sba@demodern.de>
2019-04-27 19:41:29 +0200

    fixed prefab lastname field rewrote dispensing into seperate script for reuse with customer added interface to interact with customer

--- Assets/Scripts/TellerMachine.cs
+++ Assets/Scripts/TellerMachine.cs
@@ -6,13 +6,9 @@ using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TellerMachine : MonoBehaviour {
+public class TellerMachine : MonoBehaviour, IDraggableReceiver {
+    public Dispensary dispensary;
     public Dictionary<string,float> accounts = new Dictionary<string, float>();
-    public TriggerProxy analyzerTrigger;
-    public Transform outlet;
-    public Draggable[] changePrefabs;
-    public float dispenseForce = 1f;
-    public float dispenseDelay = 0.3f;
     public string firstNameField { get; set;}
     public string lastNameField { get; set;}
     public string accountName {
@@ -30,12 +26,8 @@ public class TellerMachine : MonoBehaviour {
         }
     }
     public float balance = 0f;
-    private Queue<Draggable> dispenseQueue = new Queue<Draggable>();
-    private Coroutine dispenseRoutine;
     private void OnEnable(){
-        SortChange();
-        dispenseRoutine = StartCoroutine(DispenseRoutine());
-        analyzerTrigger.stay = (c)=>OnTriggerStay(c);
+        dispensary.RegisterReceiver(this);
     }
     private void OnDisable(){
         StopAllCoroutines();
@@ -45,55 +37,6 @@ public class TellerMachine : MonoBehaviour {
             balanceText.text = "Balance: "+accounts[accountName].ToString("C");
         }else{
             balanceText.text = "Balance: $0.00";
-        }
-    }
-
-    private void SortChange(){
-        float[] values = new float[changePrefabs.Length];
-        for(int i = 0; i < values.Length; i++){
-            values[i] = changePrefabs[i].value;
-        }
-        Array.Sort(values,changePrefabs);
-    }
-
-    private IEnumerator DispenseRoutine(){
-        while(enabled){
-            if(dispenseQueue.Count > 0){
-                var drag = dispenseQueue.Dequeue();
-                drag.gameObject.SetActive(true);
-                drag.transform.position = outlet.position;
-                drag.rigid.position = outlet.position;
-                drag.rigid.velocity = Vector3.zero;
-                drag.rigid.angularVelocity = Vector3.zero;
-                drag.rigid.AddForce(outlet.forward*dispenseForce, ForceMode.VelocityChange);
-            }
-            yield return new WaitForSeconds(dispenseDelay);
-        }
-    }
-
-    private void OnTriggerStay(Collider collider){
-        Draggable drag = collider.GetComponent<Draggable>();
-        if(drag){
-            switch(drag.type){
-                case Draggable.Type.Bill:
-                case Draggable.Type.Coin:
-                case Draggable.Type.GoldBar:
-                case Draggable.Type.SilverBar:{
-                    if(accounts.ContainsKey(accountName)){
-                        Keep(drag);
-                        Success();
-                    }else{
-                        DispenseQueued(drag);
-                        Error();
-                    }
-                    break;
-                }
-                case Draggable.Type.Generic:{
-                    DispenseQueued(drag);
-                    Error();
-                    break;
-                }
-            }
         }
     }
 
@@ -130,39 +73,40 @@ public class TellerMachine : MonoBehaviour {
         Error();
     }
 
-    private void Keep(Draggable drag){
-        balance += drag.value;  
-        Destroy(drag.gameObject);
-    }
-
-    public void Dispense(){        
-        Dispense(amountField);
-    }
-    private void Dispense(float amount){
-        float dispensed = 0;
-        if(changePrefabs.Length > 0){
-            for(int i = changePrefabs.Length-1; i >= 0;i--){
-                var prefab = changePrefabs[i];
-                while(prefab.value <= amount){
-                    Draggable drag = Instantiate<Draggable>(prefab);
-                    dispensed += drag.value;
-                    amount -= drag.value;
-                    DispenseQueued(drag);
-                }
-            }
-        }
-        balance -= dispensed;
-    }
-
-    private void DispenseQueued(Draggable drag){    
-        drag.gameObject.SetActive(false);    
-        dispenseQueue.Enqueue(drag);
-    }
-
     private void Success(){
         messageText.text = "SUCCESS";
     }
     private void Error(){
         messageText.text = "ERROR";
+    }
+    public void Dispense(){        
+        float dispensed = dispensary.DispenseChange(amountField);
+        balance -= dispensed;
+    }
+
+    public bool OnReceivedDraggable(Draggable drag)
+    {
+        switch(drag.type){
+                case Draggable.Type.Bill:
+                case Draggable.Type.Coin:
+                case Draggable.Type.GoldBar:
+                case Draggable.Type.SilverBar:{
+                    if(accounts.ContainsKey(accountName)){
+                        balance += drag.value;
+                        Success();
+                        return true;
+                    }else{
+                        Error();
+                        return false;
+                    }
+                }
+                case Draggable.Type.Generic:{
+                    Error();
+                    return false;
+                }
+                default: {
+                    throw new Exception("Unhandled Draggable!");
+                }
+            }
     }
 }
