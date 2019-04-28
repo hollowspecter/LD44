ab5adc224cad4b7e7046d52aca2e3bd3973cdcd8
Vivien Baguio <baguiov@posteo.net>
2019-04-28 19:51:30 +0200

    Implemented sound stuff

--- Assets/Scripts/SoundManagement/SoundDb.cs
+++ Assets/Scripts/SoundManagement/SoundDb.cs
@@ -8,29 +8,17 @@ namespace sfx
     [CreateAssetMenu ( fileName = "SoundDB", menuName = "ScriptableObjects/SoundDB", order = 1 )]
     public class SoundDb : ScriptableObject
     {
-        public Music Music;
         public MainSfx Main;
         public MiscSfx Misc;
-    }
-
-    [Serializable]
-    public class Music
-    {
-        public AudioClip MusicLoop;
     }
 
     [Serializable]
     public class MainSfx
     {
         public AudioClip Alarm;
-        public AudioClip Ambience;
         public AudioClip BillsCounted;
         public AudioClip CoinCounterRunning;
-        public AudioClip [] CoinDrops;
-        public AudioClip [] DoorClose;
-        public AudioClip [] DoorOpen;
         public AudioClip GunShots;
-        public AudioClip [] KeyboardClicks;
         public AudioClip MachineRunning;
         public AudioClip MouseClick;
         public AudioClip PoliceSiren;
@@ -48,6 +36,5 @@ namespace sfx
         public AudioClip TearingSound;
         public AudioClip UIAlert;
         public AudioClip BigWoosh;
-        public AudioClip Ambience;
     }
 }
