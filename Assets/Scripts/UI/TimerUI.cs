45b76c0d73ae25b135c7cbfd9ecbb84e460eb1d2
Vivien Baguio <baguiov@posteo.net>
2019-04-29 13:55:11 +0200

    Added Clock UI

---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public Text timerText;
+
+    void Update()
+    {
+        timerText.text = string.Format ( "END OF DAY IN {0}s",
+            ( App.instance.timeUntilDayEnds - App.instance.time ).ToString("0")
+            );
+    }
+}
